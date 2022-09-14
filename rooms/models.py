6197c38c85837cb72fd09b1d7ad177a9574dc7b9
June Roh <june.roh@streetsmarket.com>
2022-09-14 17:38:05 -0400

    create room model

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+
+    """Room model definition"""
+
+    class RoomKindChoices(models.TextChoices):
+        ENTIER_PLACE = ("entire_place", "Entire Place")
+        PRIVATE_ROOM = ("private_room", "Private Room")
+        SHARED_ROOM = ("shared_room", "Shared Room")
+
+    country = models.CharField(max_length=50, default="Korea")
+    city = models.CharField(max_length=80, default="Seoul")
+    price = models.PositiveIntegerField()
+    rooms = models.PositiveIntegerField()
+    toilets = models.PositiveIntegerField()
+    description = models.TextField()
+    address = models.CharField(max_length=250)
+    pet_friendly = models.BooleanField(default=False)
+    kind = models.CharField(max_length=20, choices=RoomKindChoices.choices)
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    amenities = models.ManyToManyField("rooms.Amenity")
+
+
+class Amenity(models.Model):
+
+    """Amenity Definition"""
+
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=150, null=True)
