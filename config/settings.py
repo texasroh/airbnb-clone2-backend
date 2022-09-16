be154f982750a6ae3906a91b97bc0f49b2102359
June Roh <june.roh@streetsmarket.com>
2022-09-16 09:30:57 -0400

    add direct_messages

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,7 @@ CUSTOM_APPS = [
     "wishlists.apps.WishlistsConfig",
     "bookings.apps.BookingsConfig",
     "medias.apps.MediasConfig",
+    "direct_messages.apps.DirectMessagesConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + CUSTOM_APPS
