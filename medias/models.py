5cb9883ec8a874fce43b5ac6f91d341e4e6222b4
June Roh <june.roh@streetsmarket.com>
2022-09-16 10:48:49 -0400

    change related_name for foreign keys

--- medias/models.py
+++ medias/models.py
@@ -6,7 +6,11 @@ class Photo(CommonModel):
     file = models.ImageField()
     description = models.CharField(max_length=140)
     room = models.ForeignKey(
-        "rooms.Room", on_delete=models.CASCADE, null=True, blank=True
+        "rooms.Room",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="photos",
     )
     experience = models.ForeignKey(
         "experiences.Experience", on_delete=models.CASCADE, null=True, blank=True
@@ -19,7 +23,7 @@ class Photo(CommonModel):
 class Video(CommonModel):
     file = models.FileField()
     experience = models.OneToOneField(
-        "experiences.Experience", on_delete=models.CASCADE
+        "experiences.Experience", on_delete=models.CASCADE, related_name="video"
     )
 
     def __str__(self):
