c66cdadb007435922a4d48546d979442c00edf7b
June Roh <texasroh@gmail.com>
2022-09-20 02:11:20 -0400

    add rest_framework api

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from . import models
+from .serializers import CategorySerializer
 
-# Create your views here.
+
+@api_view()
+def categories(request):
+    all_categories = models.Category.objects.all()
+    serializer = CategorySerializer(all_categories, many=True)
+    return Response({"ok": True, "categories": serializer.data})
