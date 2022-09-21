616e2c8a09981951aa01fe3502c9e35163540fb5
June Roh <june.roh@streetsmarket.com>
2022-09-21 17:34:42 -0400

    create category get api

--- categories/views.py
+++ categories/views.py
@@ -4,8 +4,19 @@ from . import models
 from .serializers import CategorySerializer
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def categories(request):
-    all_categories = models.Category.objects.all()
-    serializer = CategorySerializer(all_categories, many=True)
-    return Response({"ok": True, "categories": serializer.data})
+    if request.method == "GET":
+        all_categories = models.Category.objects.all()
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        print(request.data)
+        return Response({"created": True})
+
+
+@api_view()
+def category(request, pk):
+    category = models.Category.objects.get(pk=pk)
+    serializer = CategorySerializer(category)
+    return Response(serializer.data)
