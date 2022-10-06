db79037e209408a66873e0daefc0340ed26685d1
June Roh <texasroh@gmail.com>
2022-10-06 01:52:09 -0400

    add category when post rooms

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_404_NOT_FOUND, HTTP_204_NO_CONTENT
-from rest_framework.exceptions import NotFound, NotAuthenticated
+from rest_framework.exceptions import NotFound, NotAuthenticated, ParseError
 from .models import Amenity, Room
+from categories.models import Category
 from .serializer import AmenitySerializer, RoomListSerializer, RoomDetailSerializer
 
 
@@ -59,7 +60,16 @@ class Rooms(APIView):
             raise NotAuthenticated
         serializer = RoomDetailSerializer(data=request.data)
         if serializer.is_valid():
-            room = serializer.save(owner=request.user)
+            category_pk = request.data.get("category")
+            if not category_pk:
+                raise ParseError
+            try:
+                category = Category.objects.get(pk=category_pk)
+                if category.kind == Category.CategoryKindChoices.EXPERIENCES:
+                    raise ParseError
+            except Category.DoesNotExist:
+                raise ParseError
+            room = serializer.save(owner=request.user, category=category)
             return Response(RoomDetailSerializer(room).data)
         else:
             return Response(serializer.errors, status=HTTP_404_NOT_FOUND)
