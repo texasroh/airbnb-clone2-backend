5066bc46a157029b75fdee333c50a3ae82a7f501
June Roh <june.roh@streetsmarket.com>
2022-10-10 14:52:55 -0400

    add info parameters on custom field

--- rooms/types.py
+++ rooms/types.py
@@ -1,11 +1,13 @@
 import typing
 import strawberry
 from strawberry import auto
+from strawberry.types import Info
 from django.conf import settings
 from django.core.paginator import Paginator
 from . import models
 from users.types import UserType
 from reviews.types import ReviewType
+from wishlists.models import Wishlist
 
 
 @strawberry.django.type(models.Room)
@@ -16,7 +18,7 @@ class RoomType:
     owner: UserType
 
     @strawberry.field
-    def reviews(self, page: int = 1) -> typing.List[ReviewType]:
+    def reviews(self, page: typing.Optional[int] = 1) -> typing.List[ReviewType]:
         page_size = settings.PAGE_SIZE
         # start = (page - 1) * page_size
         # end = start + page_size
@@ -26,3 +28,13 @@ class RoomType:
     @strawberry.field
     def rating(self) -> str:
         return self.rating()
+
+    @strawberry.field
+    def is_owner(self, info: Info) -> bool:
+        return info.context.request.user == self.owner
+
+    @strawberry.field
+    def is_liked(self, info: Info) -> bool:
+        return Wishlist.objects.filter(
+            user=info.context.request.user, rooms=self
+        ).exists()
