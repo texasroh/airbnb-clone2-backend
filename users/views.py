ca3fc35a9f04510e77a2aa25720c60fa6bc89e5f
June Roh <texasroh@gmail.com>
2022-10-10 01:20:39 -0400

    add login logout change-password

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions, exceptions
 from .serializers import PrivateUserSerializer
+from .models import User
 
 
 class Me(APIView):
@@ -44,3 +46,53 @@ class Users(APIView):
         user.save()
         serializer = PrivateUserSerializer(user)
         return Response(serializer.data)
+
+
+class PublicUser(APIView):
+    def get(self, request, username):
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise exceptions.NotFound
+        serializer = PrivateUserSerializer(user)
+        return Response(serializer.data)
+
+
+class ChangePassword(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def put(self, request):
+        user = request.user
+        old_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
+        if not old_password or not new_password:
+            raise exceptions.ParseError
+        if user.check_password(old_password):
+            user.set_password(new_password)
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            raise exceptions.ParseError
+
+
+class LogIn(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            raise exceptions.ParseError
+
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            return Response({"error": "wrong password"})
+
+        login(request, user)
+        return Response({"ok": "Welcome!"})
+
+
+class LogOut(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+        return Response({"ok": "bye!"})
