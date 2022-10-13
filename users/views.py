449288cfb65b071f8cf197fe23a7a5cf81bdf156
June Roh <june.roh@streetsmarket.com>
2022-10-13 15:29:18 -0400

    add github login

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from django.conf import settings
 import jwt
+import requests
+from django.conf import settings
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
@@ -113,3 +114,51 @@ class JWTLogIn(APIView):
 
         token = jwt.encode({"pk": user.pk}, key=settings.SECRET_KEY, algorithm="HS256")
         return Response({"token": token})
+
+
+class GithubLogIn(APIView):
+    def post(self, request):
+        try:
+            code = request.data.get("code")
+            access_token = requests.post(
+                f"https://github.com/login/oauth/access_token?code={code}&client_id={settings.GITHUB_CLIENT_ID}&client_secret={settings.GITHUB_CLIENT_SECRET}",
+                headers={"Accept": "application/json"},
+            )
+            access_token = access_token.json().get("access_token")
+
+            user_data = requests.get(
+                "https://api.github.com/user",
+                headers={
+                    "Authorization": f"Bearer {access_token}",
+                    "Accept": "application/json",
+                },
+            ).json()
+            user_emails = requests.get(
+                "https://api.github.com/user/emails",
+                headers={
+                    "Authorization": f"Bearer {access_token}",
+                    "Accept": "application/json",
+                },
+            ).json()
+            email = user_emails[0]["email"]
+            for user_email in user_emails:
+                if user_email["primary"]:
+                    email = user_email["email"]
+
+            print(user_data)
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                user = User.objects.create(
+                    username=user_data.get("login"),
+                    email=email,
+                    name=user_data.get("name"),
+                    avatar=user_data.get("avatar_url"),
+                )
+                user.set_unusable_password()
+                user.save()
+            login(request, user)
+            return Response(status=status.HTTP_200_OK)
+        except Exception as e:
+            print(e)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
